740fcb5d85e0a5613b8a9557c09e535e00a312ca
kovacsv <viktorkovacs@gmail.com>
2019-01-28 20:39:56 +0100

    Polygon editor improvements.

--- Sources/VisualScriptCAD/Editor/PolygonEditor.cpp
+++ Sources/VisualScriptCAD/Editor/PolygonEditor.cpp
@@ -10,31 +10,60 @@ PolygonEditorPanel::StatusUpdater::~StatusUpdater ()
 
 PolygonEditorPanel::PolygonEditorPanel (wxWindow* parent, StatusUpdater* statusUpdater) :
 	wxPanel (parent, wxID_ANY, wxDefaultPosition, wxSize (300, 300)),
+	statusUpdater (statusUpdater),
 	polygon (),
-	statusUpdater (statusUpdater)
+	closed (false),
+	memoryBitmap (GetClientSize ()),
+	memoryDC (memoryBitmap),
+	mousePos (0, 0),
+	scale (0.01)
 {
-	SetBackgroundColour (wxColour (240, 240, 240));
+
 }
 
 void PolygonEditorPanel::OnPaint (wxPaintEvent& /*evt*/)
 {
+	wxRect clientRect = GetClientRect ();
 	wxPaintDC dc (this);
-	DrawCoordSystem (dc);
-	DrawPolygon (dc);
+	dc.Blit (0, 0, clientRect.GetWidth (), clientRect.GetHeight (), &memoryDC, 0, 0);
 }
 
 void PolygonEditorPanel::OnResize (wxSizeEvent&)
 {
-	Update ();
-	Refresh ();
+	memoryBitmap = wxBitmap (GetClientSize ());
+	memoryDC.SelectObject (memoryBitmap);
+	Draw ();
+}
+
+void PolygonEditorPanel::OnLeftClick (wxMouseEvent& evt)
+{
+	if (closed) {
+		polygon.clear ();
+		closed = false;
+	}
+	polygon.push_back (MouseCoordToPolygonPoint (evt.GetPosition ()));
+	Draw ();
 }
 
 void PolygonEditorPanel::OnMouseMove (wxMouseEvent& evt)
 {
-	statusUpdater->UpdateStatus (evt.GetX (), evt.GetY ());
+	mousePos = evt.GetPosition ();
+	statusUpdater->UpdateStatus (MouseCoordToPolygonPoint (mousePos));
+	Draw ();
 }
 
-void PolygonEditorPanel::DrawCoordSystem (wxPaintDC& dc)
+void PolygonEditorPanel::Draw ()
+{
+	memoryDC.SetPen (*wxTRANSPARENT_PEN);
+	memoryDC.SetBrush (*wxWHITE_BRUSH);
+	memoryDC.DrawRectangle (GetClientRect ());
+
+	DrawCoordSystem (memoryDC);
+	DrawPolygon (memoryDC);
+	Refresh (false);
+}
+
+void PolygonEditorPanel::DrawCoordSystem (wxDC& dc)
 {
 	wxRect clientRect = GetClientRect ();
 	dc.SetPen (wxPen (wxColour (200, 200, 200)));
@@ -48,15 +77,63 @@ void PolygonEditorPanel::DrawCoordSystem (wxPaintDC& dc)
 	);	
 }
 
-void PolygonEditorPanel::DrawPolygon (wxPaintDC& dc)
+void PolygonEditorPanel::DrawPolygon (wxDC& dc)
 {
+	if (polygon.empty ()) {
+		return;
+	}
 	std::vector<wxPoint> points;
-	points.push_back (wxPoint (10, 10));
-	points.push_back (wxPoint (100, 100));
-	points.push_back (wxPoint (100, 200));
+	for (const glm::dvec2& polygonPoint : polygon) {
+		points.push_back (PolygonPointToMouseCoord (polygonPoint));
+	}
 	dc.SetPen (wxPen (wxColour (0, 0, 0)));
-	dc.SetBrush (wxBrush (wxColour (230, 230, 230)));
-	dc.DrawPolygon (points.size (), &points[0]);
+	if (closed) {
+		dc.SetBrush (wxBrush (wxColour (230, 230, 230)));
+		dc.DrawPolygon (points.size (), &points[0]);
+	} else {
+		points.push_back (mousePos);
+		dc.DrawLines (points.size (), &points[0]);
+	}
+}
+
+glm::dvec2 PolygonEditorPanel::MouseCoordToPolygonPoint (const wxPoint& point)
+{
+	wxPoint centeredPoint = MouseCoordToCenteredCoord (point);
+	return glm::dvec2 (
+		centeredPoint.x * scale,
+		centeredPoint.y * scale
+	);
+}
+
+wxPoint PolygonEditorPanel::PolygonPointToMouseCoord (const glm::dvec2& point)
+{
+	wxPoint centeredPoint (
+		point.x / scale,
+		point.y / scale
+	);
+	return CenteredCoordToMouseCoord (centeredPoint);
+}
+
+wxPoint PolygonEditorPanel::MouseCoordToCenteredCoord (const wxPoint& point)
+{
+	wxRect clientRect = GetClientRect ();
+	int width = clientRect.GetWidth () - 2;
+	int height = clientRect.GetHeight () - 2;
+	return wxPoint (
+		(point.x - width / 2),
+		-(point.y - height / 2)
+	);
+}
+
+wxPoint PolygonEditorPanel::CenteredCoordToMouseCoord (const wxPoint& point)
+{
+	wxRect clientRect = GetClientRect ();
+	int width = clientRect.GetWidth () - 2;
+	int height = clientRect.GetHeight () - 2;
+	return wxPoint (
+		(point.x + width / 2),
+		height - (point.y + height / 2)
+	);
 }
 
 PolygonEditorDialog::StatusUpdater::StatusUpdater (PolygonEditorDialog* dialog) :
@@ -65,10 +142,10 @@ PolygonEditorDialog::StatusUpdater::StatusUpdater (PolygonEditorDialog* dialog) 
 
 }
 
-void PolygonEditorDialog::StatusUpdater::UpdateStatus (double currX, double currY)
+void PolygonEditorDialog::StatusUpdater::UpdateStatus (const glm::dvec2& position)
 {
 	std::wostringstream stream;
-	stream << std::fixed << std::setprecision (2) << currX << L", " << currY;
+	stream << std::fixed << std::setprecision (2) << position.x << L", " << position.y;
 	dialog->statusBar->SetStatusText (stream.str ());
 }
 
@@ -81,7 +158,7 @@ PolygonEditorDialog::PolygonEditorDialog (wxWindow* parent) :
 	statusBar (new wxStatusBar (this))
 {
 	boxSizer->Add (editorPanel, 1, wxEXPAND | wxALL, 0);
-	boxSizer->Add (okButton, 0, wxEXPAND | wxALL, 5);
+	boxSizer->Add (okButton, 0, wxEXPAND | wxALL, 0);
 	boxSizer->Add (statusBar, 0, wxEXPAND | wxALL, 0);
 	SetSizerAndFit (boxSizer);
 
@@ -99,6 +176,7 @@ void PolygonEditorDialog::OnButtonClick (wxCommandEvent& evt)
 BEGIN_EVENT_TABLE (PolygonEditorPanel, wxPanel)
 EVT_PAINT (PolygonEditorPanel::OnPaint)
 EVT_SIZE (PolygonEditorPanel::OnResize)
+EVT_LEFT_UP (PolygonEditorPanel::OnLeftClick)
 EVT_MOTION (PolygonEditorPanel::OnMouseMove)
 END_EVENT_TABLE ()
 
--- Sources/VisualScriptCAD/Editor/PolygonEditor.hpp
+++ Sources/VisualScriptCAD/Editor/PolygonEditor.hpp
@@ -1,6 +1,7 @@
 #ifndef POLYGONEDITOR_HPP
 #define POLYGONEDITOR_HPP
 
+#include "IncludeGLM.hpp"
 #include <wx/wx.h>
 
 class PolygonEditorPanel : public wxPanel
@@ -11,7 +12,7 @@ public:
 	public:
 		virtual ~StatusUpdater ();
 
-		virtual void UpdateStatus (double currX, double currY) = 0;
+		virtual void UpdateStatus (const glm::dvec2& position) = 0;
 	};
 
 	PolygonEditorPanel (wxWindow* parent, StatusUpdater* statusUpdater);
@@ -19,14 +20,29 @@ public:
 	void		OnPaint (wxPaintEvent& evt);
 	void		OnResize (wxSizeEvent& evt);
 
+	void		OnLeftClick (wxMouseEvent& evt);
 	void		OnMouseMove (wxMouseEvent& evt);
 
 private:
-	void		DrawCoordSystem (wxPaintDC& dc);
-	void		DrawPolygon (wxPaintDC& dc);
+	void		Draw ();
+	void		DrawCoordSystem (wxDC& dc);
+	void		DrawPolygon (wxDC& dc);
 
-	std::vector<wxPoint>	polygon;
-	StatusUpdater*			statusUpdater;
+	glm::dvec2	MouseCoordToPolygonPoint (const wxPoint& point);
+	wxPoint		PolygonPointToMouseCoord (const glm::dvec2& point);
+
+	wxPoint		MouseCoordToCenteredCoord (const wxPoint& point);
+	wxPoint		CenteredCoordToMouseCoord (const wxPoint& point);
+
+	StatusUpdater*				statusUpdater;
+
+	std::vector<glm::dvec2>		polygon;
+	bool						closed;
+
+	wxBitmap					memoryBitmap;
+	wxMemoryDC					memoryDC;
+	wxPoint						mousePos;
+	double						scale;
 
 	DECLARE_EVENT_TABLE ();
 };
@@ -39,7 +55,7 @@ public:
 	public:
 		StatusUpdater (PolygonEditorDialog* dialog);
 
-		virtual void UpdateStatus (double currX, double currY) override;
+		virtual void UpdateStatus (const glm::dvec2& position) override;
 
 	private:
 		PolygonEditorDialog* dialog;
